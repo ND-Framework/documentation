b236fdcd2fe691388acd0409030e77c85d0a1337
Hakko <67168651+Hakkodevelopment@users.noreply.github.com>
2023-10-21 14:03:27 -0700

    add meta & seo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,57 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router';
+
+
+function useHead() {
+  const { asPath } = useRouter();
+  const { frontMatter, title } = useConfig();
+  const url = `https://localhost:3000${asPath}`;
+  const description = frontMatter.description || "Documentation for ND Framework.";
+
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/x-icon" href="/static/nd.ico" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content={description} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:url" content={url} />
+    </>
+  );
+}
+
+function useNextSeoProps() {
+  const { asPath } = useRouter();
+  const arr = asPath.replace(/[-_]/g, ' ').split('/');
+  const category = (arr[1][0] !== '#' && arr[1]) || 'ND';
+  const rawTitle = arr[arr.length - 1];
+  const title = /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : '%s';
+
+  return {
+    titleTemplate: `${title} - ${
+      rawTitle === category ? 'Documentation' : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
+    }`,
+  };
+}
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <img src="https://user-images.githubusercontent.com/86536434/193913064-01b8a9b4-97a2-4bd2-9f33-99f89558ac01.png" width='40%'/>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ND-Framework',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/andys-development-857672921912836116',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/ND-Framework/documentation',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Copyright © 2023 ND Framework',
+  },
+  head: useHead,
+  useNextSeoProps: useNextSeoProps,
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
   },
 }
 
